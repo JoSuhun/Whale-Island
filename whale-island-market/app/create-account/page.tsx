d4c744d7f83686d130c7143ddf3f7a4e17079f05
josuhun <chosh9128@gmail.com>
2024-07-19 14:57:12 +0900

    Study : Validation library_zod

--- whale-island-market/app/create-account/page.tsx
+++ whale-island-market/app/create-account/page.tsx
@@ -1,51 +1,49 @@
+'use client';
+
 import FormInput from '@/components/form-input';
 import FormButton from '@/components/form-btn';
 import SocialLogin from '../../components/social-login';
+import { useFormState } from 'react-dom';
+import { createAccount } from './actions';
 
 export default function CreateAccount() {
+  const [state, action] = useFormState(createAccount, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
-        <h2 className="text-xl">
-          회원가입을 도와드릴게요
-        </h2>
+        <h2 className="text-xl">회원가입을 도와드릴게요</h2>
       </div>
-      <form className="flex flex-col gap-3">
+      <form action={action} className="flex flex-col gap-3">
         <FormInput
-          name="name"
+          name="username"
           type="text"
           placeholder="이름"
           required
-          errors={[
-            '유효한 값을 입력하세요! (한 글자 이상)',
-          ]}
+          errors={state?.fieldErrors.username}
         />
         <FormInput
           name="email"
           type="email"
           placeholder="이메일"
           required
-          errors={[]}
+          errors={state?.fieldErrors.email}
         />
         <FormInput
           name="password"
           type="password"
           placeholder="비밀번호"
           required
-          errors={[]}
+          errors={state?.fieldErrors.password}
         />
         <FormInput
-          name="password2"
+          name="confirm_password"
           type="password"
           placeholder="비밀번호 확인"
           required
-          errors={[]}
+          errors={state?.fieldErrors.confirm_password}
         />
-        <FormButton
-          loading={false}
-          text="회원가입하기"
-        />
+        <FormButton text="회원가입하기" />
       </form>
       <SocialLogin />
     </div>
