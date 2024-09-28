2aad48df973dd40257c7fd0c10fc98464f34d517
josuhun <chosh9128@gmail.com>
2024-09-28 11:34:37 +0900

    Study : 상품 등록 server action

--- whale-island-market/app/products/add/page.tsx
+++ whale-island-market/app/products/add/page.tsx
@@ -3,22 +3,46 @@ import Input from '../../../components/input';
 import Button from '../../../components/btn';
 import { PhotoIcon } from '@heroicons/react/24/solid';
 import { useState } from 'react';
+import { uploadProduct } from './actions';
+import { useFormState } from 'react-dom';
 
 export default function AddProduct() {
   const [preview, setPreview] = useState('');
-  const onImageChange = () => {};
+  const onImageChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const {
+      target: { files },
+    } = event;
+    if (!files) return;
+    const file = files[0];
+    const url = URL.createObjectURL(file);
+    setPreview(url);
+  };
+  const [state, action] = useFormState(uploadProduct, null);
+
   return (
     <div>
-      <form action="" className="flex flex-col gap-5 p-5">
+      <form
+        action={action}
+        className="flex flex-col gap-5 p-5"
+      >
         <label
           htmlFor="photo"
           className="border-2 aspect-square flex flex-col items-center justify-center
-          border-neutral-500 border-dashed rounded-md cursor-pointer"
+          border-neutral-500 border-dashed rounded-md cursor-pointer bg-cover bg-center"
+          style={{
+            backgroundImage: `url(${preview})`,
+          }}
         >
-          <PhotoIcon className="w-20" />
-          <div className="text-neutral-800 text-sm">
-            사진을 추가해주세요.
-          </div>
+          {preview === '' ? (
+            <>
+              <PhotoIcon className="w-20" />
+              <div className="text-neutral-800 text-sm">
+                사진을 추가해주세요.
+              </div>
+            </>
+          ) : null}
         </label>
         <input
           onChange={onImageChange}
@@ -32,18 +56,21 @@ export default function AddProduct() {
           required
           placeholder="제목"
           type="text"
+          errors={state?.fieldErrors.title}
         />
         <Input
           name="price"
           required
           placeholder="가격"
           type="number"
+          errors={state?.fieldErrors.price}
         />
         <Input
           name="description"
           required
           placeholder="설명"
           type="text"
+          errors={state?.fieldErrors.description}
         />
         <Button text="작성 완료" />
       </form>
