9e4bc5cfb1159d70c275a3c01ba080252b7063ea
josuhun <chosh9128@gmail.com>
2024-10-08 14:05:28 +0900

    Study : 게시글 좋아요 기능(composite id)

--- whale-island-market/app/posts/[id]/page.tsx
+++ whale-island-market/app/posts/[id]/page.tsx
@@ -1,4 +1,158 @@
-export default function Post() {
-  return <div>post</div>;
+import { notFound } from 'next/navigation';
+import db from '../../../lib/db';
+import Image from 'next/image';
+import { formatToTimeAgo } from '../../../lib/utils';
+import {
+  EyeIcon,
+  HandThumbUpIcon,
+  UserIcon,
+} from '@heroicons/react/16/solid';
+import getSession from '../../../lib/session';
+import { revalidatePath } from 'next/cache';
+
+async function getPost(id: number) {
+  try {
+    const post = await db.post.update({
+      where: {
+        id,
+      },
+      data: {
+        views: {
+          increment: 1,
+        },
+      },
+      include: {
+        user: {
+          select: {
+            username: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            comments: true,
+            likes: true,
+          },
+        },
+      },
+    });
+    return post;
+  } catch (e) {
+    return null;
+  }
+}
+
+async function getIsLiked(postId: number) {
+  const session = await getSession();
+  const like = await db.like.findUnique({
+    where: {
+      id: {
+        postId,
+        userId: session.id!,
+      },
+    },
+  });
+  return Boolean(like);
+}
+
+export default async function PostDetail({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  const id = Number(params.id);
+  if (isNaN(id)) return notFound();
+  const post = await getPost(id);
+  if (!post) return notFound();
+
+  const likePost = async () => {
+    'use server';
+
+    const session = await getSession();
+    try {
+      await db.like.create({
+        data: {
+          postId: id,
+          userId: session.id!,
+        },
+      });
+      revalidatePath(`/posts/${id}`);
+    } catch (e) {
+      return;
+    }
+  };
+
+  const dislikePost = async () => {
+    'use server';
+
+    const session = await getSession();
+    try {
+      await db.like.delete({
+        where: {
+          id: {
+            postId: id,
+            userId: session.id!,
+          },
+        },
+      });
+      revalidatePath(`/posts/${id}`);
+    } catch (e) {
+      return;
+    }
+  };
+
+  const isLiked = await getIsLiked(id);
+
+  return (
+    <div className="p-5 text-neutral-700">
+      <div className="flex items-center gap-2 mb-2">
+        <div className="size-10 overflow-hidden rounded-full">
+          {post.user.avatar !== null ? (
+            <Image
+              width={28}
+              height={28}
+              className="size-7 rounded-full"
+              src={post.user.avatar!}
+              alt={post.user.username}
+            />
+          ) : (
+            <UserIcon />
+          )}
+        </div>
+
+        <div>
+          <span className="text-sm font-semibold">
+            {post.user.username}
+          </span>
+          <div className="text-xs">
+            <span>
+              {formatToTimeAgo(post.created_at.toString())}
+            </span>
+          </div>
+        </div>
+      </div>
+      <h2 className="text-lg font-semibold">
+        {post.title}
+      </h2>
+      <p className="mb-5">{post.description}</p>
+      <div className="flex flex-col gap-5 items-start">
+        <div className="flex items-center gap-2 text-neutral-600 text-sm">
+          <EyeIcon className="size-5" />
+          <span>조회 {post.views}</span>
+        </div>
+        <form action={isLiked ? dislikePost : likePost}>
+          <button
+            className={`flex items-center gap-2 text-neutral-600 text-sm border
+                 border-neutral-500 rounded-full p-2 hover:bg-neutral-200 transition-colors`}
+          >
+            <HandThumbUpIcon className="size-5" />
+            <span>공감하기 ({post._count.likes})</span>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
